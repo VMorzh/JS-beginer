1e2861bc9c4c0683dc3acfc259250a8307ba6c6a
VladimirNote <btk.distribution@gmail.com>
2023-06-14 13:45:48 +0300

    js C/W task 12

--- index copy.js
+++ index copy.js
@@ -550,3 +550,204 @@ form: {
 // if (!hasExp) {
 //   console.log("Exp not confirm");
 // }
+// /* урок 9 Функції!!!!!!!!!!!!***********/
+
+// // function calcSpace(amount, unit = "px") {
+// //   return `${amount * 4}${unit}`;
+// // }
+
+// // console.log(calcSpace(5));
+// /* урок 9  *******
+// Рекурсія викликає сама себе в середине функції!!!!!!!!!!!***********/
+
+// function playTrackById(trackId) {
+//   //search file and launch - запустити пісню
+// }
+// // function stopTrackById(trackId) {
+// //   //отримати файл- поставити пісню на паузу
+// // }
+// function play(trackName, trackId) {
+//   console.log(`Запускаємо ${trackName}`);
+//   playTrackById(trackId);
+// }
+
+// function end(oldTrackName, newTrackName) {
+//   console.log(`Завершили грати ${oldTrackName}`);
+//   console.log(`Наступний трек ${newTrackName}`);
+// }
+
+// function pausePlay(currentTrackName) {
+//   console.log(`Трек ${currentTrackName} не на паузі`);
+//   reloadDataTrack();
+//   play();
+// }
+// function reloadDataTrack(amount) {
+//   if (amount <= 0) {
+//     console.log("Дані оновлені");
+//   } else {
+//     console.log("Повторне оновлення");
+//     // пенвий ко для оновлення данніх. Recurcia)
+//     reloadDataTrack(amount - 1);
+//   }
+// }
+// reloadDataTrack(5);
+// /* урок 9  *******
+// Замикання  Closures !!!!!!!!!!!***********/
+// function pauseStopByTrack(trackName, trackId) {
+//   let originTrackname = trackName;
+//   let originTrackId = trackId;
+
+//   return function pauseStop() {
+//     stopTrackById(originTrackId);
+//     console.log(`Трек ${originTrackname} на паузі`);
+//   };
+// }
+
+// /*сюда повертаемо функцию pauseStopByTrack и вона попадає в змінну!!!!!!!!!!!!*/
+// const pauseStop123 = function pauseStop(originTrackId, originTrackname) {
+//   stopTrackById(originTrackId);
+//   console.log(`Трек ${originTrackname} на паузі`);
+// };
+// pauseStop123();
+// const pauseStop456 = pauseStopByTrack("IT Console log");
+// pauseStop456();
+
+// /* урок 9  *******
+// Функціональний вираз ... який повертає функц callback !!!!
+// */
+
+// // const runCommand = function (command, errorFn) {
+// //   const result = command();
+// //   if (!result) {
+// //     return errorFn();
+// //   }
+// // };
+// // runCommand(
+// //   function () {
+// //     console.log("Start");
+// //     return 1 - 1;
+// //   },
+// //   function () {
+// //     console.log("Error");
+// //   }
+// // );
+
+// /*Стрілкова функція (Arrow function !!!!!!!!!!**********/
+// () => {
+//   //тут код
+// };
+// // return () => {
+// //   stopTrackById(originTrackId);
+// //   console.log(`Трек ${originTrackname} на паузі`);
+// // };
+
+// // const runCommand = function (command, errorFn) {
+// //   const result = command();
+// //   if (!result) {
+// //     return errorFn();
+// //   }
+// // };
+// // runCommand(
+// //   () => {
+// //     console.log("Start");
+// //     return 1 - 1;
+// //   },
+// //   () => console.log("Error")
+// // );
+// /* стрілкова більшь сучасніша! її варто використовувати в якості аргументів, коли вкладуємо функцію в змінну, або коли повертаємо функцію */
+// // const testFunc = () => {
+// //   let test = 10;
+// //   return test;
+// // };
+// // console.log(testFunc());
+// function stopTrackById(trackId) {
+//   //   //отримати файл- поставити пісню на паузу
+//   console.log(`отримати - постви пісні з TrackID`);
+// }
+
+/*Каррірована функція (curried function !!!!!!!!!!**********/
+// function pauseStopByTrack(trackName, trackId) {
+//   let isPause = null;
+
+//   return () => {
+//     if (isPause == true) {
+//       return;
+//     }
+
+//     stopTrackById(trackId);
+//     console.log(`Трек ${trackName} на паузі`);
+//     isPause = true;
+//   };
+// }
+
+// const pauseStop45 = pauseStopByTrack("IT Console log", 10);
+// pauseStop45();
+// pauseStop45();
+// pauseStop45();
+
+// /*Memoization /          !!!!!!!!!!**********/
+// const memoCalcSpace = (oldAmount = null, oldUnit = null, oldResult = null) => {
+//   return (amount, unit = "px") => {
+//     if (oldAmount === amount && unit === oldUnit) {
+//       console.log("memo");
+
+//       return oldResult;
+//     }
+
+//     oldResult = `${amount * 4}${unit}`;
+//     oldAmount = amount;
+//     oldUnit = unit;
+
+//     return oldResult;
+//   };
+// };
+// const calcSpace = memoCalcSpace();
+// console.log(calcSpace(4));
+// console.log(calcSpace(4));
+
+/*Композиція функцій  !!!!!!!!!!**********/
+
+const restartTrack = (trackName, trackId) => {};
+
+const memoCalcSpace = (oldAmount = null, oldUnit = null, oldResult = null) => {
+  return (amount, unit = "px") => {
+    if (oldAmount === amount && unit === oldUnit) {
+      console.log("memo");
+
+      return oldResult;
+    }
+
+    oldResult = `${amount * 4}${unit}`;
+    oldAmount = amount;
+    oldUnit = unit;
+
+    return oldResult;
+  };
+};
+const calcSpace = memoCalcSpace();
+console.log(calcSpace(4));
+console.log(calcSpace(4));
+
+const getSpaceFromDesign = (componentName) => {
+  switch (componentName) {
+    case "button":
+      return 4;
+    case "card":
+      return 3;
+    default:
+      return 2;
+  }
+};
+const isMobile = true;
+
+const amount = getSpaceFromDesign("button");
+const amountMobile = isMobile ? amount / 2 : amount;
+const amountInpx = calcSpace(amountMobile);
+
+const calcSpaceFromDesign = (componentName) => {
+  const result = getSpaceFromDesign(componentName);
+
+  return calcSpace(isMobile ? result / 2 : result);
+};
+console.log("///////");
+console.log(calcSpaceFromDesign("button"));
--- index.js
+++ index.js
@@ -1,201 +1,162 @@
-// /* урок 9 Функції!!!!!!!!!!!!***********/
+/*урок 11 вбудовані глобальні функции*/
 
-// // function calcSpace(amount, unit = "px") {
-// //   return `${amount * 4}${unit}`;
-// // }
+// const code = `console.log("Hello world")`;
+// eval(code);
 
-// // console.log(calcSpace(5));
-// /* урок 9  *******
-// Рекурсія викликає сама себе в середине функції!!!!!!!!!!!***********/
+// function calcScreenRation(w, h) {
+//   let result = w / h;
 
-// function playTrackById(trackId) {
-//   //search file and launch - запустити пісню
+//   return isFinite(result) ? result : null;
 // }
-// // function stopTrackById(trackId) {
-// //   //отримати файл- поставити пісню на паузу
-// // }
-// function play(trackName, trackId) {
-//   console.log(`Запускаємо ${trackName}`);
-//   playTrackById(trackId);
-// }
+// console.log(calcScreenRation(1920, "123"));
 
-// function end(oldTrackName, newTrackName) {
-//   console.log(`Завершили грати ${oldTrackName}`);
-//   console.log(`Наступний трек ${newTrackName}`);
-// }
+// function calcScreenRation(w, h) {
+//   let result = w / h;
 
-// function pausePlay(currentTrackName) {
-//   console.log(`Трек ${currentTrackName} не на паузі`);
-//   reloadDataTrack();
-//   play();
-// }
-// function reloadDataTrack(amount) {
-//   if (amount <= 0) {
-//     console.log("Дані оновлені");
+//   if (isNaN(result)) {
+//     return "Error";
+//   } else if (!isFinite(result)) {
+//     return "is Infinity";
 //   } else {
-//     console.log("Повторне оновлення");
-//     // пенвий ко для оновлення данніх. Recurcia)
-//     reloadDataTrack(amount - 1);
+//     return result;
 //   }
 // }
-// reloadDataTrack(5);
-// /* урок 9  *******
-// Замикання  Closures !!!!!!!!!!!***********/
-// function pauseStopByTrack(trackName, trackId) {
-//   let originTrackname = trackName;
-//   let originTrackId = trackId;
+// console.log(calcScreenRation(1920, 0));
 
-//   return function pauseStop() {
-//     stopTrackById(originTrackId);
-//     console.log(`Трек ${originTrackname} на паузі`);
-//   };
+// const num = parseInt("12px");
+// console.log(num);
+
+// function calcScreenRation(w, h) {
+//   w = parseInt(w);
+//   h = parseInt(h);
+
+//   let result = w / h;
+
+//   if (isNaN(result)) {
+//     return "Error";
+//   } else if (!isFinite(result)) {
+//     return "is Infinity";
+//   } else {
+//     return result;
+//   }
+// }
+// console.log(calcScreenRation("1920px", "1000px"));
+// const num = parseFloat("1.555");
+// console.log(num);
+
+/*урок 12 вбудовані можливості для числових типів*/
+// const a = Number(10);
+// const b = Number.();
+// a.toFixed
+// console.log(a === b);
+
+// let a = 0.1 + 0.2;
+// let b = 0.3;
+
+// if (a - b < Number.EPSILON && b - a < Number.EPSILON) {
+//   console.log("a i b практично рівні");
+// } else {
+//   console.log("a i b не рівні");
 // }
 
-// /*сюда повертаемо функцию pauseStopByTrack и вона попадає в змінну!!!!!!!!!!!!*/
-// const pauseStop123 = function pauseStop(originTrackId, originTrackname) {
-//   stopTrackById(originTrackId);
-//   console.log(`Трек ${originTrackname} на паузі`);
-// };
-// pauseStop123();
-// const pauseStop456 = pauseStopByTrack("IT Console log");
-// pauseStop456();
+// const a = 0.00001;
+// const b = 0.00002;
+// const c = 0.00003;
 
-// /* урок 9  *******
-// Функціональний вираз ... який повертає функц callback !!!!
-// */
+// const d = a + b;
+// console.log(d);
 
-// // const runCommand = function (command, errorFn) {
-// //   const result = command();
-// //   if (!result) {
-// //     return errorFn();
-// //   }
-// // };
-// // runCommand(
-// //   function () {
-// //     console.log("Start");
-// //     return 1 - 1;
-// //   },
-// //   function () {
-// //     console.log("Error");
-// //   }
-// // );
+// console.log(d - c < Number.EPSILON && c - d < Number.EPSILON);
 
-// /*Стрілкова функція (Arrow function !!!!!!!!!!**********/
-// () => {
-//   //тут код
-// };
-// // return () => {
-// //   stopTrackById(originTrackId);
-// //   console.log(`Трек ${originTrackname} на паузі`);
-// // };
+// const a = 0.00051;
+// const b = 0.00052;
+// const c = 0.00103;
 
-// // const runCommand = function (command, errorFn) {
-// //   const result = command();
-// //   if (!result) {
-// //     return errorFn();
-// //   }
-// // };
-// // runCommand(
-// //   () => {
-// //     console.log("Start");
-// //     return 1 - 1;
-// //   },
-// //   () => console.log("Error")
-// // );
-// /* стрілкова більшь сучасніша! її варто використовувати в якості аргументів, коли вкладуємо функцію в змінну, або коли повертаємо функцію */
-// // const testFunc = () => {
-// //   let test = 10;
-// //   return test;
-// // };
-// // console.log(testFunc());
-// function stopTrackById(trackId) {
-//   //   //отримати файл- поставити пісню на паузу
-//   console.log(`отримати - постви пісні з TrackID`);
+// const d = a + b;
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// const a = 0.01;
+// const b = 0.02;
+// const c = 0.03;
+
+// const d = a + b;
+
+// console.log(Number.MAX_VALUE);
+
+// console.log(d - c < Number.MIN_VALUE && c - d < Number.EPSILON);
+
+/*Перервірка на ЦІЛЕ число !!!!!*/
+
+// function reviewNumber(num) {
+//   if (isNaN(num)) {
+//     return console.log("Is Not a number", num);
+//   }
+
+//   if (!num && num !== 0) {
+//     return console.log("Bad number", num);
+//   }
+
+//   if (!Number.isInteger(num)) {
+//     return console.log("Fraction number", num);
+//   }
+//   return console.log(num);
+// }
+// reviewNumber("10.5");
+
+// function reviewNumber(num) {
+//   if (isNaN(num)) {
+//     return console.log("Is Not a number", num);
+//   }
+//   if (!num && num !== 0) {
+//     return console.log("Bad number", num);
+//   }
+//   if (!Number.isInteger(num)) {
+//     return console.log("Fraction number", num);
+//   }
+//   if (!Number.isSafeInteger(num)) {
+//     return console.log("Danger number", num);
+//   }
+//   return console.log(num);
 // }
 
-/*Каррірована функція (curried function !!!!!!!!!!**********/
-// function pauseStopByTrack(trackName, trackId) {
-//   let isPause = null;
+// reviewNumber(Number.MAX_SAFE_INTEGER + 12);
 
-//   return () => {
-//     if (isPause == true) {
-//       return;
-//     }
+/* Вбудовані можливсті для!!! числ типів*/
 
-//     stopTrackById(trackId);
-//     console.log(`Трек ${trackName} на паузі`);
-//     isPause = true;
-//   };
-// }
+// const a1 = Number(10);
+// const a2 = a1.toFixed();
+// Number(100).toFixed;
 
-// const pauseStop45 = pauseStopByTrack("IT Console log", 10);
-// pauseStop45();
-// pauseStop45();
-// pauseStop45();
+// (10).toExponential;
+// 1000;
+// 10000;
 
-// /*Memoization /          !!!!!!!!!!**********/
-// const memoCalcSpace = (oldAmount = null, oldUnit = null, oldResult = null) => {
-//   return (amount, unit = "px") => {
-//     if (oldAmount === amount && unit === oldUnit) {
-//       console.log("memo");
+/* Number - більш технічний, старий формат створення числа, */
+/* краще використати глобалФункцію parseInt() */
+// const a1 = 1.025501;
+// const a2 = parseInt("1000 f");
+// const a3 = parseFloat(a1.toFixed(3));
+// /*to Fixed превращает число в рядок, ParseFloat поверне нам десяткове число*/
+// console.log(typeof a3, a3);
 
-//       return oldResult;
-//     }
+// const a1 = 1253.45601;
+// const a2 = parseFloat(a1.toPrecision(2));
+// console.log(a2);
 
-//     oldResult = `${amount * 4}${unit}`;
-//     oldAmount = amount;
-//     oldUnit = unit;
+// const a1 = 999989125345601;
+// const a2 = parseInt(a1);
+// const a3 = a1.toExponential();
+// const a4 = parseFloat(a3);
+// console.log(a3, a4);
 
-//     return oldResult;
-//   };
-// };
-// const calcSpace = memoCalcSpace();
-// console.log(calcSpace(4));
-// console.log(calcSpace(4));
+// const a1 = 94567;
+// const a2 = a1.toExponential();
+// const a3 = parseFloat(a2);
+// console.log(a1 === a3);
 
-/*Композиція функцій  !!!!!!!!!!**********/
-
-const restartTrack = (trackName, trackId) => {};
-
-const memoCalcSpace = (oldAmount = null, oldUnit = null, oldResult = null) => {
-  return (amount, unit = "px") => {
-    if (oldAmount === amount && unit === oldUnit) {
-      console.log("memo");
-
-      return oldResult;
-    }
-
-    oldResult = `${amount * 4}${unit}`;
-    oldAmount = amount;
-    oldUnit = unit;
-
-    return oldResult;
-  };
-};
-const calcSpace = memoCalcSpace();
-console.log(calcSpace(4));
-console.log(calcSpace(4));
-
-const getSpaceFromDesign = (componentName) => {
-  switch (componentName) {
-    case "button":
-      return 4;
-    case "card":
-      return 3;
-    default:
-      return 2;
-  }
-};
-const isMobile = true;
-
-const amount = getSpaceFromDesign("button");
-const amountMobile = isMobile ? amount / 2 : amount;
-const amountInpx = calcSpace(amountMobile);
-
-const calcSpaceFromDesign = (componentName) => {
-  const result = getSpaceFromDesign(componentName);
-
-  return calcSpace(isMobile ? result / 2 : result);
-};
-console.log("///////");
-console.log(calcSpaceFromDesign("button"));
+// const a1 = 94567;
+// const a2 = a1.toExponential();
+// const a3 = parseFloat(a2);
+// const a4 = a3.toString(32);
+// console.log(a4);
